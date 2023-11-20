b9bf91fd00a079bc85d8e03654437134922e85b3
dory <minhduc.xd.93@gmail.com>
2023-11-20 15:12:02 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 import subprocess
+app = FastAPI()
 
-my_fastapi_app = FastAPI()
 
-
-@my_fastapi_app.get("/")
+@app.get("/")
 async def root():
     result = subprocess.check_output('docker exec -it ichiran-main-1 ichiran-cli -f "一覧は最高だぞ"', shell=True)
     result = result.decode('utf-8')
