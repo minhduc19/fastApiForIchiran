d2392a65bc8e73d92a778afb1f92a812c6debf3e
Dory Nguyen <minhduc.xd.93@gmailc.om>
2023-11-20 12:17:45 +0000

    fist ichiran command

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+import subprocess
 
 my_fastapi_app = FastAPI()
 
 
 @my_fastapi_app.get("/")
 async def root():
-    return {"Nginx": "I'm alive"}
+    result = subprocess.check_output('docker exec -it ichiran-main-1 ichiran-cli -f "一覧は最高だぞ"', shell=True)
+    result = result.decode('utf-8')
+    return {"Nginx": result}
